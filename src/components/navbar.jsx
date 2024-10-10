8d76018921784c797df55e4ef5fdc6ac4b89352e
Ramazan Serhat UYGUN <92759220+Rsuygun@users.noreply.github.com>
2024-10-10 03:09:12 +0300

    Navbar is Uptaded

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,6 +2,7 @@
 
 import Link from "next/link";
 import { useState } from "react";
+import Image from "next/image";
 
 const links = [
   { url: "/", title: "Ana Sayfa" },
@@ -14,8 +15,16 @@ const Navbar = () => {
   const [open, setOpen] = useState(false);
   return (
     <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+      {/* Links */}
+      <div className="hidden md:flex gap-4 ">
+        {links.map((link) => (
+          <Link href={link.url} key={link.url}>
+            {link.title}
+          </Link>
+        ))}
+      </div>
       {/* Logo */}
-      <div className="md:hidden">
+      <div className="md:hidden lg:flex w-1/3 justify-center">
         <Link
           href="/"
           className="text-sm bg-gray-950 rounded-md p-1 font-semibold flex items-center justify-center"
@@ -26,15 +35,30 @@ const Navbar = () => {
           </span>
         </Link>
       </div>
+      {/* Social */}
+      <div className="hidden md:flex gap-4 w-1/4">
+        <Link href="https://github.com/Rsuygun">
+          <Image src="/github.png" alt="" width={24} height={24} />
+        </Link>
+        <Link href="/">
+          <Image src="/linkedin.png" alt="" width={24} height={24} />
+        </Link>
+        <Link href="/">
+          <Image src="/instagram.png" alt="" width={24} height={24} />
+        </Link>
+      </div>
       {/* Responsive Menu */}
       <div className="md:hidden">
-        <button className="w-10 h-8 flex flex-col justify-between z-50 relative" onClick={()=>setOpen((prev) => !prev)}>
+        <button
+          className="w-10 h-8 flex flex-col justify-between z-50 relative"
+          onClick={() => setOpen((prev) => !prev)}
+        >
           <div className="w-10 h-1 bg-gray-500 rounded"></div>
           <div className="w-10 h-1 bg-gray-500 rounded"></div>
           <div className="w-10 h-1 bg-gray-500 rounded"></div>
         </button>
         {/* Menu List */}
-        {open &&
+        {open && (
           <div className="absolute top-0 left-0 w-screen h-screen bg-gray-950 text-gray-500 flex flex-col items-center justify-center gap-10 text-4xl">
             {links.map((link) => (
               <Link href={link.url} key={link.url}>
@@ -42,7 +66,7 @@ const Navbar = () => {
               </Link>
             ))}
           </div>
-        }
+        )}
       </div>
     </div>
   );
